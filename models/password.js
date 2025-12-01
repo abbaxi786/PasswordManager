57de7764dcc53c6de5c2d192277a91a2d3f3494e
abbaxi786 <141753795+abbaxi786@users.noreply.github.com>
2025-12-01 17:12:50 +0500

    Add encryption and frontend changes

--- models/password.js
+++ models/password.js
@@ -1,13 +1,35 @@
 import mongoose from "mongoose";
+import CryptoJS from "crypto-js";
 
 const passwordSchema = new mongoose.Schema({
   name: { type: String, required: true },
   websiteURL: { type: String, required: true },
   icon: { type: String },
   email: { type: String, required: true },
+
+  // Will be stored encrypted
   password: { type: String, required: true },
-  user: { type: String, required: true }, // you can use ObjectId if referencing User
+
+  user: { type: String, required: true },
 });
 
-// Export the model
-export default mongoose.models.Password || mongoose.model("Password", passwordSchema);
+// Encrypt BEFORE saving
+passwordSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  const key = process.env.PASSWORD_ENCRYPTION_KEY;
+
+  this.password = CryptoJS.AES.encrypt(this.password, key).toString();
+  next();
+});
+
+// Decrypt AFTER retrieving
+passwordSchema.methods.decryptPassword = function () {
+  const key = process.env.PASSWORD_ENCRYPTION_KEY;
+
+  const bytes = CryptoJS.AES.decrypt(this.password, key);
+  return bytes.toString(CryptoJS.enc.Utf8);
+};
+
+export default mongoose.models.Password ||
+  mongoose.model("Password", passwordSchema);
